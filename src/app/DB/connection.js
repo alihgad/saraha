a9b9becc9d92ff0134517c6b36e007268fb2890e
ali <alihgad2@gmail.com>
2024-07-05 15:15:04 +0300

    connect

--- src/app/DB/connection.js
+++ src/app/DB/connection.js
@@ -1,7 +1,7 @@
-import { default as mongoose } from "mongoose";
+import mongoose from "mongoose";;
 
 let connect = async ()=>{
-    return await mongoose.connect('mongodb://127.0.0.1:27017/saraha').then(()=>{
+    return await mongoose.connect('mongodb://localhost:27017/saraha').then(()=>{
         console.log('Connected to MongoDB');
     }).catch(err => {
         console.error('Failed to connect to MongoDB', err);
