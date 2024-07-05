552d03bcb5f2f1677de6d82c14d147fbe4d13423
ali <alihgad2@gmail.com>
2024-07-05 04:28:49 +0300

    local

--- app/DB/connection.js
+++ app/DB/connection.js
@@ -1,6 +1,6 @@
 import { default as mongoose } from "mongoose";
 
-let connect = mongoose.connect('mongodb://localhost:27017/saraha').then(()=>{
+let connect = mongoose.connect('mongodb://127.0.0.1:27017/saraha').then(()=>{
     console.log('Connected to MongoDB');
 }).catch(err => {
     console.error('Failed to connect to MongoDB', err);
