c425b00737a1e8ae5fea9087bd184b1c3f883d1e
ali <alihgad2@gmail.com>
2024-07-06 20:34:31 +0300

    uri

--- src/app/DB/connection.js
+++ src/app/DB/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";;
 
 let connect = async ()=>{
-    return await mongoose.connect('mongodb://localhost:27017/saraha').then(()=>{
+    return await mongoose.connect('mongodb+srv://alihgad2:w3rlw2u4YMO785xE@bookshub.4uutypb.mongodb.net/assignment-8').then(()=>{
         console.log('Connected to MongoDB');
     }).catch(err => {
         console.error('Failed to connect to MongoDB', err);
